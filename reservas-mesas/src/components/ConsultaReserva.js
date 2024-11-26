71f852c5aa12db8a14c861ab8d7cdaafaedfd715
Igor Teodoro <igor.teodoro@estrela.com.br>
2024-11-26 14:03:41 -0300

    Consultar reservas

---
+++ reservas-mesas/src/components/ConsultaReserva.js
@@ -0,0 +1,30 @@
+// src/components/FinalizarComanda.js
+import React, { useState } from 'react';
+import api from '../api';
+
+const ConsultaReserva = () => {
+
+    const [message, setMessage] = useState('');
+
+    const handleConsultaReserva = async () => {
+        try {
+            const response = await api.post('/ConsultarReserva', {
+                action: 'consultarReserva',
+
+            });
+            setMessage(response.data.body);
+        } catch (error) {
+            setMessage('Erro ao consultar mesas.');
+        }
+    };
+
+    return (
+        <div>
+            <h2>Consultar mesas</h2>
+            <button onClick={handleConsultaReserva}>Finalizar</button>
+            <p>{message}</p>
+        </div>
+    );
+};
+
+export default ConsultaReserva;
